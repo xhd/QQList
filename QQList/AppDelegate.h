ffe3126687a16a2e523d6efe74d845c5abfb3d49
Carol923 <ningwang23@163.com>
2014-11-22 11:18:03 +0800

    Initial Commit

---
+++ QQList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQList
+//
+//  Created by CarolWang on 14/11/22.
+//  Copyright (c) 2014年 CarolWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QQList
+//
+//  Created by CarolWang on 14/11/22.
+//  Copyright (c) 2014年 CarolWang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
